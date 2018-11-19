9c93d8fdbe1585541c64e525279946398adbdba1
charliesands <cwsands3@gmail.com>
2018-11-19 15:44:53 -0700

    form setup

--- LabelBouncer/Form1.cs
+++ LabelBouncer/Form1.cs
@@ -16,5 +16,25 @@ namespace LabelBouncer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
     }
 }
